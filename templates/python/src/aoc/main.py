5ae50222cc73ecb901c09361a3d6481421531c16
Dan Fornika <dfornika@gmail.com>
2019-12-02 10:26:02 -0800

    Update python template to use argparse

--- templates/python/src/aoc/main.py
+++ templates/python/src/aoc/main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 
-def main():
-    for line in sys.stdin:
-        print(line.strip())
+def main(args):
+    with open(args.input) as f:
+        for line in f:
+            print(line.strip())
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", dest="input", help="input data")
+    args = parser.parse_args()
+    main(args)
