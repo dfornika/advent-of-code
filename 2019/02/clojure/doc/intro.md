95f4ed17f3bc740b5ea6ea81c992e09357302a0e
Dan Fornika <dfornika@gmail.com>
2019-12-08 09:11:58 -0800

    Re-organizing directory structure

--- 2019/02/clojure/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to aoc.2019.02.part-1
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- 2019/02/clojure/test/aoc/2019/02/part_1_test.clj
+++
@@ -1,7 +0,0 @@
-(ns aoc.2019.02.part-1-test
-  (:require [clojure.test :refer :all]
-            [aoc.2019.02.part-1 :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
