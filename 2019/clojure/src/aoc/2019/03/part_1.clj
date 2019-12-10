0e39c1e41266a3b9a128c056fe66074c94d936aa
Dan Fornika <dfornika@gmail.com>
2019-12-09 22:46:15 -0800

    Correct solution to day 3 part 1

--- 2019/clojure/src/aoc/2019/03/part_1.clj
+++ 2019/clojure/src/aoc/2019/03/part_1.clj
@@ -6,7 +6,8 @@
 (defn move-right
   ""
   [initial-state distance]
-  (let [new-xs (range (inc (get-in initial-state [:position :x])) (inc distance))
+  (let [initial-x (get-in initial-state [:position :x])
+        new-xs (range (inc initial-x) (+ initial-x (inc distance)))
         y (get-in initial-state [:position :y])
         new-points (set (map #(assoc {:y y} :x %) new-xs))]
     (-> initial-state
@@ -16,7 +17,8 @@
 (defn move-down
   ""
   [initial-state distance]
-  (let [new-ys (range (- distance) (get-in initial-state [:position :y]))
+  (let [initial-y (get-in initial-state [:position :y])
+        new-ys (range (- initial-y distance) initial-y)
         x (get-in initial-state [:position :x])
         new-points (set (map #(assoc {:x x} :y %) new-ys))]
     (-> initial-state
@@ -26,7 +28,8 @@
 (defn move-left
   ""
   [initial-state distance]
-  (let [new-xs (range (- distance) (get-in initial-state [:position :x]))
+  (let [initial-x (get-in initial-state [:position :x])
+        new-xs (range (- initial-x distance) initial-x)
         y (get-in initial-state [:position :y])
         new-points (set (map #(assoc {:y y} :x %) new-xs))]
     (-> initial-state
@@ -36,7 +39,8 @@
 (defn move-up
   ""
   [initial-state distance]
-  (let [new-ys (range (inc (get-in initial-state [:position :y])) (inc distance))
+  (let [initial-y (get-in initial-state [:position :y])
+        new-ys (range (inc initial-y) (+ initial-y (inc distance)))
         x (get-in initial-state [:position :x])
         new-points (set (map #(assoc {:x x} :y %) new-ys))]
     (-> initial-state
@@ -49,8 +53,7 @@
         y1 (:y p1)
         x2 (:x p2)
         y2 (:y p2)]
-    (+ (Math/abs (- x2 x1)) (Math/abs (- y2 y1)))
-    ))
+    (+ (Math/abs (- x2 x1)) (Math/abs (- y2 y1)))))
 
 (defn add-points
   [initial-state movement]
@@ -64,13 +67,11 @@
       (= direction \L)
       (move-left initial-state distance)
       (= direction \U)
-      (move-up initial-state distance)
-      )))
+      (move-up initial-state distance))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  ;; (def input-file "../input_1.txt")
+  ;; (def input-file "../inputs/input_03.txt")
   (def input-file *in*)
   (def wire-paths (mapv #(str/split % #",") (str/split (slurp input-file) #"\n")))
 
@@ -79,50 +80,26 @@
                       :points #{}})
 
   (def wire-1
-    (loop [state initial-state
-           path (first wire-paths)]
-      (if (empty? path)
-        (:points state)
-        (recur (add-points state (first path))
-               (rest path)))))
+    (:points (reduce add-points initial-state (first wire-paths))))
 
   (def wire-2
-    (loop [state initial-state
-           path (second wire-paths)]
-      (if (empty? path)
-        (:points state)
-        (recur (add-points state (first path))
-               (rest path)))))
+    (:points (reduce add-points initial-state (second wire-paths))))
 
-  (prn (set/intersection wire-1 wire-2))
-  #_(println (apply min (map (partial manhattan-distance origin) (set/intersection wire-1 wire-2)))))
+  (println (apply min (map (partial manhattan-distance origin) (set/intersection wire-1 wire-2)))))
 
 (comment
+  (def wire-1-path ["R8" "U5" "L5" "D3"])
+  (def wire-2-path ["U7" "R6" "D4" "L4"])
   (def wire-1-path ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"])
   (def wire-2-path ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
+  (def wire-1-path ["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"])
+  (def wire-2-path ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"])
   (def origin {:x 0 :y 0})
   (def initial-state  {:position origin :points #{}})
   (def wire-1
-    (-> initial-state
-      (add-points "R75")
-      (add-points "D30")
-      (add-points "R83")
-      (add-points "U83")
-      (add-points "L12")
-      (add-points "D49")
-      (add-points "R71")
-      (add-points "U7")
-      (add-points "L72")))
+    (reduce add-points initial-state wire-1-path))
   (def wire-2
-    (-> initial-state
-        (add-points "U62")
-        (add-points "R66")
-        (add-points "U55")
-        (add-points "R34")
-        (add-points "D71")
-        (add-points "R55")
-        (add-points "D58")
-        (add-points "R83")))
-  (map (partial manhattan-distance origin) (set/intersection (:points wire-1) (:points wire-2)))
+    (reduce add-points initial-state wire-2-path))
+  (apply min (map (partial manhattan-distance origin) (set/intersection (:points wire-1) (:points wire-2))))
   
   )
